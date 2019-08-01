c841fd805bb15e3927366bfdfd9e23eb6ccf2c0c
duhaime <douglas.duhaime@gmail.com>
2019-08-01 14:55:37 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup (
+  name='yale-facenet',
+  version='0.0.1',
+  packages=['facenet'],
+  keywords = ['machine-learning', 'tensorflow', 'facenet', 'computer-vision'],
+  description='Python implementation of the facenet model',
+  url='https://github.com/yaledhlab/yale-facenet',
+  author='David Sandberg, packaged by Douglas Duhaime',
+  author_email='douglas.duhaime@gmail.com',
+  license='MIT',
+  install_requires=[
+
+  ],
+)
